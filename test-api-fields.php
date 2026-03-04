6f2b5aae1530528723915bb5e1a29f79ef808fa7
fred0939 <kouadiofrejus777@gmail.com>
2026-03-04 16:43:31 +0000

    Remove originals after moving to help

--- test-api-fields.php
+++
@@ -1,7 +0,0 @@
-<?php
-$json = json_decode(file_get_contents('http://localhost/APP_IB/backend/Api/Sales/list.php'), true);
-echo "Champs disponibles:\n";
-print_r(array_keys($json['data'][0]));
-echo "\nPremière vente:\n";
-print_r($json['data'][0]);
-?>
